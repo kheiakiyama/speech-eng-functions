b1437547221cb12616fe17ddcac56a87b0d5ab81
kheiakiyama <kheiakiyama@gmail.com>
2025-08-23 09:19:32 +0900

    :+1: update: migrate Speechs

---
+++ QuestionEntity.cs
@@ -0,0 +1,94 @@
+using System;
+using Azure;
+using Azure.Data.Tables;
+
+public class QuestionEntity : Azure.Data.Tables.ITableEntity
+{
+    public QuestionEntity(ulong id)
+    {
+        this.PartitionKey = "speech-eng";
+        this.RowKey = id.ToString();
+        ResultCount = 0;
+        CorrectCount = 0;
+    }
+
+    public QuestionEntity() { }
+
+    public string Sentence { get; set; }
+    public int ResultCount { get; set; }
+    public int CorrectCount { get; set; }
+    public string PartitionKey { get; set; }
+    public string RowKey { get; set; }
+    public DateTimeOffset? Timestamp { get; set; }
+    public ETag ETag { get; set; }
+
+    // public static QuestionEntity GetEntity(string id)
+    // {
+    //     CloudTable table = GetTable();
+    //     TableOperation retrieveOperation = TableOperation.Retrieve<QuestionEntity>("speech-eng", id);
+    //     TableResult retrievedResult = table.Execute(retrieveOperation);
+    //     return (QuestionEntity)retrievedResult.Result;
+    // }
+
+    // public static QuestionEntity GetEntity(DateTime time, ILogger log)
+    // {
+    //     log.Info($"time:{time.ToString()}");
+    //     CloudTable table = GetTable();
+    //     TableQuery<QuestionEntity> query = new TableQuery<QuestionEntity>().Where(
+    //         TableQuery.CombineFilters(
+    //             TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, "speech-eng"),
+    //             TableOperators.And,
+    //             TableQuery.GenerateFilterConditionForDate("Timestamp", QueryComparisons.LessThan, time)));
+    //     query.TakeCount = 20;
+    //     var entity = RandomChoise(table.ExecuteQuery(query).ToArray());
+    //     if (entity != null)
+    //     {
+    //         log.Info($"EntityTime:{entity.Timestamp.ToString()}");
+    //         return (QuestionEntity)entity;
+    //     }
+
+    //     query = new TableQuery<QuestionEntity>().Where(
+    //             TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, "speech-eng"));
+    //     query.TakeCount = 20;
+    //     entity = RandomChoise(table.ExecuteQuery(query).ToArray());
+    //     if (entity != null)
+    //     {
+    //         log.Info($"Entity2Time:{entity.Timestamp.ToString()}");
+    //         return (QuestionEntity)entity;
+    //     }
+
+    //     return null;
+    // }
+
+    private static QuestionEntity RandomChoise(QuestionEntity[] entities)
+    {
+        var random = new Random();
+        var index = random.Next(entities.Length);
+        return index < entities.Length ? entities[index] : null;
+    }
+
+    // private static CloudTable tmpTable = null;
+
+    // private static CloudTable GetTable()
+    // {
+    //     if (tmpTable != null)
+    //         return tmpTable;
+    //     CloudStorageAccount storageAccount = CloudStorageAccount.Parse(Environment.GetEnvironmentVariable("speechengfunction_STORAGE"));
+    //     CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+    //     return tmpTable = tableClient.GetTableReference("sentences");
+    // }
+
+    // public void Replace()
+    // {
+    //     CloudTable table = GetTable();
+    //     TableOperation updateOperation = TableOperation.Replace(this);
+    //     table.Execute(updateOperation);
+    // }
+
+    // public void Insert()
+    // {
+    //     CloudTable table = GetTable();
+    //     TableOperation insertOperation = TableOperation.Insert(this);
+    //     table.Execute(insertOperation);
+    // }
+}
---
+++ Speechs.cs
@@ -0,0 +1,80 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.Extensions.Logging;
+
+namespace SpeechEng.Functions;
+
+public class Speechs
+{
+    private readonly ILogger<Speechs> _logger;
+
+    public Speechs(ILogger<Speechs> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function("Speechs")]
+    [BlobOutput("speechs/{queueTrigger}.wav")]
+    public async Task<byte[]> Run(
+        [QueueTrigger("speech")] string queueItem,
+        [TableInput("sentences", "speech-eng", "{queueTrigger}")] QuestionEntity entity)
+    {
+        speechBinary = null;
+        _logger.LogInformation($"Queue trigger function processed: {queueItem}\n" +
+        $"rowKey={entity.RowKey}");
+
+        string accessToken;
+        _logger.LogInformation(Environment.GetEnvironmentVariable("BingSpeechKey"));
+        var speechConfig = SpeechConfig.FromSubscription(Environment.GetEnvironmentVariable("BingSpeechKey"), "en-US");
+        using var speechSynthesizer = new SpeechSynthesizer(speechConfig, null);
+
+        var result = await speechSynthesizer.SpeakTextAsync(entity.Sentence);
+        using var audioStream = AudioDataStream.FromResult(result);
+        var memoryStream = new MemoryStream();
+
+        byte[] buffer = new byte[4096];
+        uint bytesRead;
+        while ((bytesRead = audioStream.ReadData(buffer)) > 0)
+        {
+            memoryStream.Write(buffer, 0, (int)bytesRead);
+        }
+        return memoryStream.ToArray();
+    }
+
+    private Stream ConvertToWaveFormat(MemoryStream rawAudioStream)
+    {
+        rawAudioStream.Position = 0;
+        using var reader = new BinaryReader(rawAudioStream);
+        using var wavStream = new MemoryStream();
+
+        using var writer = new BinaryWriter(wavStream);
+        int sampleRate = 16000;
+        int bitsPerSample = 16;
+        int channels = 1;
+
+        int byteRate = sampleRate * channels * (bitsPerSample / 8);
+        int dataSize = (int)rawAudioStream.Length;
+
+        writer.Write("RIFF".ToCharArray());
+        writer.Write(36 + dataSize);
+        writer.Write("WAVE".ToCharArray());
+        writer.Write("fmt ".ToCharArray());
+        writer.Write(16);
+        writer.Write((short)1);
+        writer.Write((short)channels);
+        writer.Write(sampleRate);
+        writer.Write(byteRate);
+        writer.Write((short)(channels * (bitsPerSample / 8)));
+        writer.Write((short)bitsPerSample);
+        writer.Write("data".ToCharArray());
+        writer.Write(dataSize);
+        rawAudioStream.Position = 0;
+        rawAudioStream.CopyTo(wavStream);
+        wavStream.Position = 0;
+        return new MemoryStream(wavStream.ToArray());
+    }
+
+    private static Stream? speechBinary = null;
+}
