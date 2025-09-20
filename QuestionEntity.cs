c37c2d30c95ecf78d26a181b85e6bb3ff34f8ec8
kheiakiyama <kheiakiyama@gmail.com>
2025-09-20 16:00:43 +0900

    :+1:update: rewrite with .net8

--- QuestionEntity.cs
+++ QuestionEntity.cs
@@ -1,6 +1,11 @@
 using System;
+using Aspire.Hosting;
 using Azure;
 using Azure.Data.Tables;
+using Aspire.Azure.Data.Tables;
+using Azure.Data.Tables.Models;
+using Microsoft.Extensions.Logging;
+using SpeechEng.Functions;
 
 public class QuestionEntity : Azure.Data.Tables.ITableEntity
 {
@@ -22,43 +27,39 @@ public class QuestionEntity : Azure.Data.Tables.ITableEntity
     public DateTimeOffset? Timestamp { get; set; }
     public ETag ETag { get; set; }
 
-    // public static QuestionEntity GetEntity(string id)
-    // {
-    //     CloudTable table = GetTable();
-    //     TableOperation retrieveOperation = TableOperation.Retrieve<QuestionEntity>("speech-eng", id);
-    //     TableResult retrievedResult = table.Execute(retrieveOperation);
-    //     return (QuestionEntity)retrievedResult.Result;
-    // }
+    public static QuestionEntity GetEntity(string id)
+    {
+        var table = GetTable();
+        var entity = table.GetEntityIfExists<QuestionEntity>("speech-eng", id);
+        return entity.Value;
+    }
 
-    // public static QuestionEntity GetEntity(DateTime time, ILogger log)
-    // {
-    //     log.Info($"time:{time.ToString()}");
-    //     CloudTable table = GetTable();
-    //     TableQuery<QuestionEntity> query = new TableQuery<QuestionEntity>().Where(
-    //         TableQuery.CombineFilters(
-    //             TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, "speech-eng"),
-    //             TableOperators.And,
-    //             TableQuery.GenerateFilterConditionForDate("Timestamp", QueryComparisons.LessThan, time)));
-    //     query.TakeCount = 20;
-    //     var entity = RandomChoise(table.ExecuteQuery(query).ToArray());
-    //     if (entity != null)
-    //     {
-    //         log.Info($"EntityTime:{entity.Timestamp.ToString()}");
-    //         return (QuestionEntity)entity;
-    //     }
-
-    //     query = new TableQuery<QuestionEntity>().Where(
-    //             TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, "speech-eng"));
-    //     query.TakeCount = 20;
-    //     entity = RandomChoise(table.ExecuteQuery(query).ToArray());
-    //     if (entity != null)
-    //     {
-    //         log.Info($"Entity2Time:{entity.Timestamp.ToString()}");
-    //         return (QuestionEntity)entity;
-    //     }
-
-    //     return null;
-    // }
+    public static QuestionEntity GetEntity(DateTime time, ILogger<Questions> log)
+    {
+        log.LogInformation($"time:{time.ToString()}");
+        var table = GetTable();
+        var res = table.Query<QuestionEntity>()
+            .Where(e => e.PartitionKey == "speech-eng" && e.Timestamp < time)
+            .Take(20)
+            .ToArray();
+        var entity = RandomChoise(res);
+        if (entity != null)
+        {
+            log.LogInformation($"EntityTime:{entity.Timestamp.ToString()}");
+            return entity;
+        }
+        res = table.Query<QuestionEntity>()
+            .Where(e => e.PartitionKey == "speech-eng")
+            .Take(20)
+            .ToArray();
+        entity = RandomChoise(res);
+        if (entity != null)
+        {
+            log.LogInformation($"Entity2Time:{entity.Timestamp.ToString()}");
+            return entity;
+        }
+        return null;
+    }
 
     private static QuestionEntity RandomChoise(QuestionEntity[] entities)
     {
@@ -67,23 +68,23 @@ public class QuestionEntity : Azure.Data.Tables.ITableEntity
         return index < entities.Length ? entities[index] : null;
     }
 
-    // private static CloudTable tmpTable = null;
+    private static TableClient tmpTable = null;
 
-    // private static CloudTable GetTable()
-    // {
-    //     if (tmpTable != null)
-    //         return tmpTable;
-    //     CloudStorageAccount storageAccount = CloudStorageAccount.Parse(Environment.GetEnvironmentVariable("speechengfunction_STORAGE"));
-    //     CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
-    //     return tmpTable = tableClient.GetTableReference("sentences");
-    // }
+    private static TableClient GetTable()
+    {
+        if (tmpTable != null)
+            return tmpTable;
 
-    // public void Replace()
-    // {
-    //     CloudTable table = GetTable();
-    //     TableOperation updateOperation = TableOperation.Replace(this);
-    //     table.Execute(updateOperation);
-    // }
+        var serviceClient = new TableServiceClient(Environment.GetEnvironmentVariable("speechengfunction_STORAGE"));
+        tmpTable = serviceClient.GetTableClient("sentences");
+        return tmpTable;
+    }
+
+    public void Replace()
+    {
+        var table = GetTable();
+        table.UpdateEntity(this, ETag.All, TableUpdateMode.Replace);
+    }
 
     // public void Insert()
     // {
---
+++ Questions.cs
@@ -0,0 +1,154 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace SpeechEng.Functions;
+
+public class Questions
+{
+    private readonly ILogger<Questions> _logger;
+
+    public Questions(ILogger<Questions> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function("Questions")]
+    public Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
+        if (req.Method == "GET")
+        {
+            return Get(req);
+        }
+        else
+        {
+            return Post(req);
+        }
+    }
+
+private async Task<IActionResult> Get(HttpRequest req)
+{
+    string timeText = req.Query["time"];
+    dynamic data = await req.ReadFromJsonAsync<object>();
+    timeText = timeText ?? data?.id;
+
+    if (timeText == null)
+        return new BadRequestObjectResult(new { error = "Please pass time on the query string or in the request body." });
+
+    if (!DateTime.TryParse(timeText, out DateTime time))
+        return new BadRequestObjectResult(new { error = "Parse time failed." });
+
+    var question = QuestionEntity.GetEntity(time, _logger);
+    if (question == null)
+    {        
+        var customResponse = new
+        {
+            Code = 500,
+            Message = "Internal Server Error",
+            ErrorMessage = "This is a bug, maybe.."
+        };
+        return new ObjectResult(customResponse) 
+        { 
+            StatusCode = StatusCodes.Status500InternalServerError 
+        };
+    }
+
+    return new ObjectResult(new
+    {
+        id = question.RowKey,
+        sentence = question.Sentence,
+        total = question.ResultCount,
+        correct = question.CorrectCount,
+        url = $"https://speechengfunction.blob.core.windows.net/speechs/{question.RowKey}.wav",
+        time = question.Timestamp
+    });
+}
+
+private async Task<IActionResult> Post(HttpRequest req)
+{
+    string id = req.Query["id"];
+    string sentence = req.Query["sentence"];
+    dynamic data = await req.ReadFromJsonAsync<object>();
+    id = id ?? data?.id;
+    sentence = sentence ?? data?.sentence;
+
+    if (id == null || sentence == null)
+        return new BadRequestObjectResult(new { error = "Please pass a id and sentence on the query string or in the request body." });
+
+    var question = QuestionEntity.GetEntity(id);
+    if (question == null)
+    {
+        var customResponse = new
+        {
+            Code = 500,
+            Message = "Internal Server Error",
+            ErrorMessage = "This is a bug, maybe.."
+        };
+        return new ObjectResult(customResponse) 
+        { 
+            StatusCode = StatusCodes.Status500InternalServerError 
+        };
+    }
+    question.ResultCount += 1;
+    var cos = calculate(question.Sentence, sentence);
+    _logger.LogInformation($"cos:{cos}");
+    var perfect = double.Parse(Environment.GetEnvironmentVariable("BORDER_PERFECT"));
+    var good = double.Parse(Environment.GetEnvironmentVariable("BORDER_GOOD"));
+    string comment;
+    if (cos > perfect)
+    {
+        question.CorrectCount += 1;
+        comment = "PERFECT!!";
+    }
+    else if (cos > good)
+    {
+        question.CorrectCount += 1;
+        comment = "GOOD!";
+    }
+    else
+        comment = "OOPS...";
+
+    question.Replace();
+    return new OkObjectResult(new { cos, comment });
+}
+
+private double calculate(string text1, string text2)
+{
+    var ar1 = breakUp(text1);
+    var ar2 = breakUp(text2);
+    var uniques = ar1.Concat(ar2).Distinct().ToArray();
+    var flgs1 = make_flags(uniques, ar1.Distinct().ToArray());
+    var flgs2 = make_flags(uniques, ar2.Distinct().ToArray());
+    return dot(flgs1, flgs2, uniques.Length) / (double)uniques.Length;
+}
+
+    private string[] breakUp(string text)
+    {
+    throw new NotImplementedException();
+    // using (var tagger = MeCabTagger.Create(Environment.GetEnvironmentVariable("MeCabDicDir")))
+        // {
+        //     var node = tagger.Parse(text);
+        //     var ret = new List<string>();
+        //     while (node != null)
+        //     {
+        //         ret.Add(node.Surface);
+        //         log.LogInformation(node.Surface);
+        //         node = node.Next;
+        //     }
+        //     return ret.ToArray();
+        // }
+    }
+
+private static int[] make_flags(string[] uniques, string[] elements)
+{
+    return uniques.Select(word => elements.Contains(word) ? 1 : 0).ToArray();
+}
+
+private static int dot(int[] i1, int[] i2, int length)
+{
+    return Enumerable.Range(0, length).Sum(i => i1[i] * i2[i]);
+}
+}
